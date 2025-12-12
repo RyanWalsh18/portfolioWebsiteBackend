726abb1a8041ef05b746a1d2be525324cb3ae19c
Ryan Walsh <ed20rw2@leeds.ac.uk>
2025-12-12 22:27:32 +0000

    GET 'project' -> return all of the projects

--- backend/src/main/java/com/portfolio_website/backend/controller/ProjectController.java
+++ backend/src/main/java/com/portfolio_website/backend/controller/ProjectController.java
@@ -1,4 +1,31 @@
 package com.portfolio_website.backend.controller;
 
+
+import com.portfolio_website.backend.entity.Project;
+import com.portfolio_website.backend.service.ProjectService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//Project endpoint
+@RestController
+@RequestMapping("/project")
 public class ProjectController {
+
+    //create instance of ProjectService
+    private final ProjectService projectService;
+
+    //Constructor
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    // GET "/project"
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
 }
--- backend/src/main/java/com/portfolio_website/backend/service/ProjectService.java
+++ backend/src/main/java/com/portfolio_website/backend/service/ProjectService.java
@@ -1,4 +1,24 @@
 package com.portfolio_website.backend.service;
 
+
+import com.portfolio_website.backend.entity.Project;
+import com.portfolio_website.backend.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProjectService {
+    //initialise Customer Repo
+    private final ProjectRepository projectRepository;
+
+    //Constructor
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    //return all projects
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
 }
